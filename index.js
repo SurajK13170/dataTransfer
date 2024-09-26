b4919813dfb1000cb5bac5220fa1666881f77da6
SurajK13170 <bgrills57@gmail.com>
2024-09-26 13:08:05 +0530

    done

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const port = 8080
+
+app.post('/dataPushFunction', async (req, res) => {
+    const { dataPushUrl, requestBody } = req.body;
+    try {
+        console.log("Pushing data to", dataPushUrl);
+        const response = await axios.post(dataPushUrl, requestBody, {
+            headers: {
+                accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+        });
+        console.log("Data push response:", response.data);
+        res.status(200).json({ status: "success", data: response.data });
+    } catch (error) {
+        console.log("Error pushing data:", error);
+        res.status(500).json({ status: "failure", error: error.message });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
